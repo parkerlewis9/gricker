f25249c6b8485a777f8ec4639082ac97a698b9d2
415DomSmith <415.dominic.smith@gmail.com>
2016-09-24 00:08:53 -0500

    changes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = new express();
 
 app.get('/', function(req, res){
-  res.render('./../app/index.ejs')
+  res.render('./../app/index.html')
 })
-.listen(7777);
+.listen(7777)
