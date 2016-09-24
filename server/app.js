80820817385e576cc622655c6ee0568c7a5c9296
Parker Lewis <parker.lewis14@att.net>
2016-09-24 00:28:09 -0500

    initial setup of gulp

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = new express();
 
-app.set('views', '../app')
+app.set('views', './app')
 app.set("view engine", "ejs")
 
 app.get('/', function(req, res){
-  res.render('index', {})
+  res.render('index.ejs', {})
 })
 .listen(7777);
